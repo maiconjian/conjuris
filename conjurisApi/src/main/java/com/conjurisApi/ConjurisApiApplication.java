85788d90c9ddfd92c22900405cc157de24e14c10
maiconjian <maiconjiantelles@gmail.com>
2020-05-28 02:34:56 -0300

    primeiro

---
+++ conjurisApi/src/main/java/com/conjurisApi/ConjurisApiApplication.java
@@ -0,0 +1,13 @@
+package com.conjurisApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjurisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjurisApiApplication.class, args);
+	}
+
+}
---
+++ conjurisApi/src/test/java/com/conjurisApi/ConjurisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conjurisApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConjurisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
